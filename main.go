79d2bb6b34280e616177333aa3d5a0362f3afe77
Eyjan-Huang <eyjanhuang@gmail.com>
2022-11-17 16:08:01 -0800

    adding the conditional file into the project

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	f "flag"
+	"fmt"
+)
+
+var (
+	mode       = f.String("mode", "server", "Please specify either <client> or <server>")
+	serverInfo = f.String("server-addr", "127.0.0.1:5001", "server's address with port number, specifically")
+	clientInfo = f.String("peer-addrs", "127.0.0.1:5001,127.0.0.1:5002,127.0.0.1:5003", "client's address with port number, using comma to seperate, no space")
+)
 
 func main() {
-	slogan := "Hello Go World!"
-	fmt.Println(slogan)
+	f.Parse()
+
+	switch *mode {
+	case "server":
+		fmt.Println("This is server")
+	case "client":
+		fmt.Println("This is client")
+	}
 }
